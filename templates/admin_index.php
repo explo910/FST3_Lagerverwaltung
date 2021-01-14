d2504f8e1c9f4f200008965930f09f2b7b551b95
Georg <georg.pretzner@gmail.com>
2021-01-14 21:16:00 +0100

    date

--- templates/admin_index.php
+++ templates/admin_index.php
@@ -1,6 +1,24 @@
+
 <div class="wrap">
-    <h2>Dies ist die Lagerverwaltung</h2>
+    <h3>Willkommen bei der GetYourCake Lagerverwaltung!</h3>
 </div>
-<div class="wrap">
-    <h3>Willkommen bei der GetYourCake Lagerverwaltung - verwenden sie die Navigation auf der Linken seite!</h3>
-<?php
+
+
+</br></br></br></br></br></br>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-3">
+      <a href="admin.php?page=gyc_lakt"><h3>Lagerstand verwalten</h3></a>
+    </div>
+    <div class="col-3">
+      <a href="admin.php?page=gyc_ltrans"><h3>Transaktionshistory</h3></a>
+    </div>
+    <div class="col-3">
+    </div>
+  </div>
+</div>
+
+
+
--- templates/admin_ltrans.php
+++ templates/admin_ltrans.php
@@ -1,50 +1,106 @@
 <div class="wrap">
-    <h2>Dies wird die Lagertransaktionsliste</h2>
+  <h1>Lagertransaktionsliste - Spechtelliste
+  </h1>
 </div>
 
 
- <?php
-            global $wpdb;
-            $table_hist = $wpdb->prefix . "hist";
-
-            $retrieve_data = $wpdb->get_results
-            ( 
-                "SELECT hist.histID, hist.timestmp, hist.person, hist.change_log, hist.change_type
-                FROM $table_hist AS `hist` 
-            );
-        ?>
+</br>
 
 <div class="container; col-8">
-    <div class="row">
-
-          <table class="table">
-            <thead>
-                <tr>
-                <th style="font-weight: bold;text-align: left" scope="col">History ID</th>
-                <th style="font-weight: bold;text-align: left" scope="col">Datum / Zeitpunk</th>
-                <th style="font-weight: bold;text-align: left" scope="col">Person</th>
-                <th style="font-weight: bold;text-align: left" scope="col">Art</th>
-                <th style="font-weight: bold;text-align: left" scope="col">Änderung</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    foreach ($retrieve_data as $retrieved_data)
-                    { 
-                        ?>
-                        <tr>
-                            <td style="text-align: left"><?php echo $retrieved_data->histID;?></td>
-                            <td style="text-align: left"><?php echo $retrieved_data->timestmp;?></td>
-                            <td style="text-align: left"><?php echo $retrieved_data->person;?></td>
-                            <td style="text-align: left"><?php echo $retrieved_data->change_log;?></td>
-                            <td style="text-align: left"><?php echo $retrieved_data->change_type;?></td>
-                            <td style="text-align: left">Weitere Einträge Laden</td>
-                            <td style="text-align: left"><a href="admin.php?page=gyc_lakt&id=<?php echo $retrieved_data->ID;?>">aktualisieren</a></td>
-                        </tr>
-                        <?php 
-                    }
-                ?>
-            </tbody>
-        </table>
+    <div class="col">
+      <form method="post" action="admin.php?page=gyc_ltrans">
+      <div class="row-4">
+         Von: <input type="date" name="von">
+         Bis: <input type="date" name="bis">
+      </div>
+      <div class="row-3">
+        Anzahl Eintr&auml;ge: 
+        <input type="text" name="lines">
+        <input type="submit" value="aktualisieren"> 
+        </div>
+      </form>
     </div>
+
+</br>
+
+  <div class="row">
+    <table class="table">
+      <thead>
+        <tr>
+          <th style="font-weight: bold;text-align: left" scope="col">History ID
+          </th>
+          <th style="font-weight: bold;text-align: left" scope="col">Datum / Zeitpunk
+          </th>
+          <th style="font-weight: bold;text-align: left" scope="col">Person
+          </th>
+          <th style="font-weight: bold;text-align: left" scope="col">Art
+          </th>
+          <th style="font-weight: bold;text-align: left" scope="col">&Auml;nderung
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+       <?php
+            $count = 50;
+            if ($_SERVER["REQUEST_METHOD"] == "POST")
+            {
+                if (isset($_POST['lines']) and $_POST['lines'] > 0)
+                {
+                    $count = $_POST['lines'];
+                }
+                if (isset($_POST['von']) and $_POST['von'] > 0)
+                {
+                    $von = $_POST['von'];
+                }
+                if (isset($_POST['bis']) and $_POST['bis'] > 0)
+                {
+                    $bis = $_POST['bis'];
+                }
+
+                global $wpdb;
+                $table_hist = "hist";
+                if (isset($von) && isset($bis))
+                {
+                    $retrieve_data = $wpdb->get_results("SELECT hist.histID, hist.timestmp, hist.person, hist.change_log, hist.change_type
+            FROM $table_hist AS `hist` where hist.timestmp between \'$von\' and \'$bis\' order by hist.histID desc");
+                }
+                else
+                {
+                    $retrieve_data = $wpdb->get_results("SELECT hist.histID, hist.timestmp, hist.person, hist.change_log, hist.change_type
+            FROM $table_hist AS `hist` order by hist.histID desc");
+                }
+
+            }
+            $counter = 2;
+            foreach ($retrieve_data as $retrieved_data)
+            {
+            ?>
+
+        <tr>
+          <td style="text-align: left">
+            <?php echo $retrieved_data->histID;?>
+          </td>
+          <td style="text-align: left">
+            <?php echo $retrieved_data->timestmp;?>
+          </td>
+          <td style="text-align: left">
+            <?php echo $retrieved_data->person;?>
+          </td>
+          <td style="text-align: left">
+            <?php echo $retrieved_data->change_log;?>
+          </td>
+          <td style="text-align: left">
+            <?php echo $retrieved_data->change_type;?>
+          </td>
+        </tr>
+        <?php
+            if ($counter>$count) {
+                break;
+            }
+                $counter++;
+            }
+            ?>
+      </tbody>
+    </table>
+  </div>
 </div>
